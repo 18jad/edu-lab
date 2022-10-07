1f45b774671022f14065157eec541e3a28fe5311
Jad Yahya <jadoyahya@gmail.com>
2022-10-07 23:39:21 +0300

    [App]: Added index page elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div></div>;
-}
+import styles from "./App.module.scss";
+import Logo from "./components/Logo";
+import UserSelection from "./components/UserSelection";
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <Logo />
+      </header>
+      <div className={styles.selectionWrapper}>
+        <UserSelection type='student' />
+        <UserSelection type='admin' />
+        <UserSelection type='instructor' />
+      </div>
+    </div>
+  );
+};
 
 export default App;
