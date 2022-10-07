e7fb0262ccfd26ff9a13153faabb04b6c35ff675
Jad Yahya <jadoyahya@gmail.com>
2022-10-08 01:40:09 +0300

    [Pages]: Created admin login page and elements

--- frontend/src/pages/AdminLogin.tsx
+++ frontend/src/pages/AdminLogin.tsx
@@ -1,5 +1,12 @@
+import LoginForm from "../components/LoginForm";
+import styles from "../styles/LoginPages.module.scss";
+
 const AdminLogin = () => {
-  return <div>AdminLogin</div>;
+  return (
+    <div className={styles.wrapper}>
+      <LoginForm type='Admin' />
+    </div>
+  );
 };
 
 export default AdminLogin;
