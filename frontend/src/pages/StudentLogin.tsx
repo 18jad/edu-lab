477ec42a898fd19b7062bff16596583441fa1ffb
Jad Yahya <jadoyahya@gmail.com>
2022-10-08 01:36:34 +0300

    [Pages]: Created student login page and elements

--- frontend/src/pages/StudentLogin.tsx
+++ frontend/src/pages/StudentLogin.tsx
@@ -1,5 +1,15 @@
+import LoginForm from "../components/LoginForm";
+import styles from "../styles/LoginPages.module.scss";
+
 const StudentLogin = () => {
-  return <div>StudentLogin</div>;
+  return (
+    <div className={styles.wrapper}>
+      <LoginForm type='Student' />
+      <p className={styles.warn}>
+        *If you are new talk to admin to create an account for you
+      </p>
+    </div>
+  );
 };
 
 export default StudentLogin;
