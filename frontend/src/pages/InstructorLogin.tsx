b30a72844e430b5c5c0904219ddd513191be916c
Jad Yahya <jadoyahya@gmail.com>
2022-10-08 01:43:22 +0300

    [Pages]: Created instructor login page and elements

--- frontend/src/pages/InstructorLogin.tsx
+++ frontend/src/pages/InstructorLogin.tsx
@@ -1,5 +1,15 @@
+import LoginForm from "../components/LoginForm";
+import styles from "../styles/LoginPages.module.scss";
+
 const InstructorLogin = () => {
-  return <div>InstructorLogin</div>;
+  return (
+    <div className={styles.wrapper}>
+      <LoginForm type='Instructor' />
+      <p className={styles.warn}>
+        *If you are new talk to admin to create an account for you
+      </p>
+    </div>
+  );
 };
 
 export default InstructorLogin;
