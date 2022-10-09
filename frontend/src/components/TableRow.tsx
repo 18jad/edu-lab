226ed969682daefff4e7d7b953bedfe47a9d23d4
Jad Yahya <jadoyahya@gmail.com>
2022-10-09 09:07:24 +0300

    [Components]: Improved table row component

--- frontend/src/components/TableRow.tsx
+++ frontend/src/components/TableRow.tsx
@@ -1,10 +1,32 @@
 import styles from "../styles/AdminStudentsPage.module.scss";
 
-const TableRow = ({ id, name }: any) => {
+const TableRow = ({ id, name, action, actionId, title }: any) => {
   return (
     <div className={styles.row}>
-      <p className={styles.rowId}>#{id}</p>
-      <p className={styles.rowName}>{name}</p>
+      {id ? <p className={styles.rowId}>#{id}</p> : ""}
+      {title ? <p className={styles.rowId}>{title}</p> : ""}
+      <p className={styles.rowName} title={name}>
+        {name}
+      </p>
+      {action ? (
+        <>
+          <input
+            type='file'
+            name='assigmentSubmit'
+            id='assigmentSubmit'
+            accept='.txt, .docx'
+            hidden
+          />
+          <label
+            htmlFor='assigmentSubmit'
+            className={styles.actionButton}
+            data-id={actionId}>
+            {action}
+          </label>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
