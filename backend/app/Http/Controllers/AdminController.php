ff5d5b81dc503857bb1ac0de979df62de12f7760
Jad Yahya <jadoyahya@gmail.com>
2022-10-09 22:46:19 +0300

    [Controller]: Added admin login method

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -3,13 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
+
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'error' => $validator->errors()
+            ]);
+        } else {
+            $admin_credentials = $request->only('username', 'password');
+            $auth_token = Auth::attempt($admin_credentials);
+            return !$auth_token ? response()->json([
+                'status' => false,
+                'message' => 'Username or password incorrect'
+            ]) : response()->json([
+                'status' => true,
+                'admin' => Auth::user(),
+                'authorization' => [
+                    'auth_token' => $auth_token,
+                    'type' => 'Bearer',
+                ]
+            ]);
+        }
+    }
+
     public function addStudent(Request $request): \Illuminate\Http\JsonResponse
     {
-
         $student = new Student;
         if (isset($request->name, $request->username, $request->password)) {
             $already_exists = Student::where('username', $request->username);
