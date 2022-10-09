b484de91ac15211f01f67f9f7b19411a03910eba
Jad Yahya <jadoyahya@gmail.com>
2022-10-09 22:34:34 +0300

    [Routes]: Addde required routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+//     Route::post('login', [AuthController::class, 'login'])->name('login');
+//     Route::post('register', [AuthController::class, 'register']);
+//     Route::post('logout', [AuthController::class, 'logout']);
+// });
+
+
+Route::group(["prefix" => 'v1.0'], function() {
+    Route::group(['prefix' => 'student'], function() {
+        Route::controller(StudentsController::class)->group(function () {
+            Route::post('login', 'login');
+            Route::middleware(['auth:api'])->group(function() {
+                Route::get('logout', 'logout');
+                Route::post('submit_assignment', 'submitAssignment');
+                Route::get('courses', 'getEnrolledCourses');
+            });
+        });
+    });
+    Route::group(['prefix' => 'admin'], function() {
+        Route::controller(AdminController::class)->group(function () {
+            Route::post('login', 'login');
+//            Route::middleware(['auth:api'])->group(function() {
+                Route::get('logout', 'logout');
+                Route::post('add_student', 'addStudent');
+                Route::post('add_instructor', 'addInstructor');
+//            });
+        });
+    });
 });
