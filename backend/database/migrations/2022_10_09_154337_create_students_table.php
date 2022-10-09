ecce45fd53986dbab8a5e2f7c4e9f23ece7dc217
Jad Yahya <jadoyahya@gmail.com>
2022-10-09 19:04:25 +0300

    [Migrations]: Created students table migration

---
+++ backend/database/migrations/2022_10_09_154337_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    protected $connection = 'mongodb';
+
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("username")->unique();
+            $table->string("password");
+            $table->string("enrolled_courses");
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
