ab7e38578446ca0baea6b5d4daffed4a99c0023d
Jad Yahya <jadoyahya@gmail.com>
2022-10-09 08:02:22 +0300

    [App]: Added instructor protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,11 @@ import AdminDashboard from "./pages/AdminDashboard";
 import AdminInstructor from "./pages/AdminInstructor";
 import AdminLogin from "./pages/AdminLogin";
 import AdminStudents from "./pages/AdminStudents";
+import InstructorAnnouncements from "./pages/InstructorAnnouncements";
+import InstructorAssignments from "./pages/InstructorAssignments";
+import InstructorDashboard from "./pages/InstructorDashboard";
 import InstructorLogin from "./pages/InstructorLogin";
+import InstructorStudents from "./pages/InstructorStudents";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
 import StudentLogin from "./pages/StudentLogin";
@@ -55,6 +59,42 @@ const App = () => {
       </Route>
       <Route path='/instructor'>
         <Route path='login' element={<InstructorLogin />} />
+        <Route
+          path='dashboard'
+          element={
+            <ProtectedRoute
+              user={true}
+              redirect='../login'
+              access={<InstructorDashboard />}
+            />
+          }></Route>
+        <Route
+          path='dashboard/students'
+          element={
+            <ProtectedRoute
+              user={true}
+              redirect='../login'
+              access={<InstructorStudents />}
+            />
+          }></Route>
+        <Route
+          path='dashboard/assignments'
+          element={
+            <ProtectedRoute
+              user={true}
+              redirect='../login'
+              access={<InstructorAssignments />}
+            />
+          }></Route>
+        <Route
+          path='dashboard/announcements'
+          element={
+            <ProtectedRoute
+              user={true}
+              redirect='../login'
+              access={<InstructorAnnouncements />}
+            />
+          }></Route>
       </Route>
       <Route path='/student'>
         <Route path='login' element={<StudentLogin />} />
