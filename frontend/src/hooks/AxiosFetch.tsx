9e1ed008d1f3e5002c20d53ccca94dc9e53377de
Jad Yahya <jadoyahya@gmail.com>
2022-10-12 01:59:29 +0300

    [Hook]: Added instructor axios instance

--- frontend/src/hooks/AxiosFetch.tsx
+++ frontend/src/hooks/AxiosFetch.tsx
@@ -4,4 +4,8 @@ const admin = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1.0/admin",
 });
 
-export { admin };
+const instructor = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1.0/instructor",
+});
+
+export { admin, instructor };
