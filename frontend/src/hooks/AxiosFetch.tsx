5dc82c641bd63f7258e0d2d3b21fc80338486894
Jad Yahya <jadoyahya@gmail.com>
2022-10-12 12:52:11 +0300

    [Hooks]: Added student axios instance

--- frontend/src/hooks/AxiosFetch.tsx
+++ frontend/src/hooks/AxiosFetch.tsx
@@ -14,4 +14,11 @@ const instructor = axios.create({
   },
 });
 
-export { admin, instructor };
+const student = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1.0/student",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("student_access_token")}`,
+  },
+});
+
+export { admin, instructor, student };
