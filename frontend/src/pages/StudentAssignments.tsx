b36861fb918e833a71e0b25e58a466904205f8fc
Jad Yahya <jadoyahya@gmail.com>
2022-10-12 13:10:18 +0300

    [Linking]: Linked get assignments

--- frontend/src/pages/StudentAssignments.tsx
+++ frontend/src/pages/StudentAssignments.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
 import Sidebar from "../components/Sidebar";
 import TableRow from "../components/TableRow";
+import { student } from "../hooks/AxiosFetch";
 import styles from "../styles/AdminStudentsPage.module.scss";
 
+interface Assignment {
+  assignment_id: string;
+  assignment_title: string;
+  assignment_body: string;
+}
+
+interface Assignments {
+  assignments: any;
+}
+
 const StudentAssignments = () => {
+  const [assignments, setAssignments] = useState<Assignment[]>([]);
+
+  useEffect(() => {
+    student.get<Assignments>("/assignments").then((response) => {
+      let data = response.data;
+      setAssignments(data.assignments[0].assignments);
+    });
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <Sidebar type='student' />
@@ -15,11 +37,14 @@ const StudentAssignments = () => {
               <p>Content</p>
               <p>Action</p>
             </div>
-            <TableRow
-              name='Do grid table'
-              title='CSS Homework'
-              action='Submit'
-            />
+            {assignments.map((assignment) => (
+              <TableRow
+                name={assignment.assignment_body}
+                title={assignment.assignment_title}
+                key={assignment.assignment_id}
+                action='Submit'
+              />
+            ))}
           </div>
         </div>
       </main>
