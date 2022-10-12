71ccf5d3b68497f36cfb4da1db7d66c8db7eb601
Jad Yahya <jadoyahya@gmail.com>
2022-10-12 12:19:20 +0300

    [Hooks]: Adjusted axios instance (added auth header)

--- frontend/src/hooks/AxiosFetch.tsx
+++ frontend/src/hooks/AxiosFetch.tsx
@@ -2,10 +2,16 @@ import axios from "axios";
 
 const admin = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1.0/admin",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("admin_access_token")}`,
+  },
 });
 
 const instructor = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1.0/instructor",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("instructor_access_token")}`,
+  },
 });
 
 export { admin, instructor };
