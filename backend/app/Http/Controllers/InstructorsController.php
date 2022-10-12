34bcb8c3a200062c729b97ee5c6057df298b8dfb
Jad Yahya <jadoyahya@gmail.com>
2022-10-12 12:21:16 +0300

    [Fix]: Fixed issue in checking student

--- backend/app/Http/Controllers/InstructorsController.php
+++ backend/app/Http/Controllers/InstructorsController.php
@@ -9,7 +9,6 @@ use App\Models\Student;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
-use PhpParser\Node\Expr\Array_;
 
 
 class InstructorsController extends Controller
@@ -52,7 +51,7 @@ class InstructorsController extends Controller
 
             // check if student is available
             $student = Student::where('username', $student_username)->first();
-            if (!$student->count()) {
+            if (!$student || !$student->count()) {
                 return response()->json([
                     'status' => false,
                     'message' => "Student doesn't exits"
